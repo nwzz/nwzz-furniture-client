94f7bd46e339f663538c0a6c5184b5d69358d8ec
nwzz <shahnewaz@mail.com>
2022-03-12 20:33:58 +0600

    env update

--- build/static/js/0.f404ade1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-boilarplate"]=this["webpackJsonpreact-boilarplate"]||[]).push([[0],{344:function(t,a,r){"use strict";r.d(a,"a",(function(){return c}));var e=r(0);function c(t){return Object(e.a)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"}}]})(t)}}}]);
-//# sourceMappingURL=0.f404ade1.chunk.js.map
